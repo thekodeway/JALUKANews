4faa3f85556aca660d5aff8ef4bcfed9f9f2446e
thekodeway <thekodeway@gmail.com>
2021-04-01 09:50:08 +0530

    Uploading 1st Commit

---
+++ lib/helper/news.dart
@@ -0,0 +1,66 @@
+import 'package:http/http.dart' as http;
+import 'package:flutternews/models/article.dart';
+import 'dart:convert';
+import 'package:flutternews/secret.dart';
+import '../models/article.dart';
+
+class News {
+  List<Article> news = [];
+
+  Future<void> getNews() async {
+    String url =
+        "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=$apiKey";
+
+    var response = await http.get(Uri.parse(url));
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          Article article = Article(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            urlToImage: element['urlToImage'],
+            publshedAt: DateTime.parse(element['publishedAt']),
+            content: element["content"],
+            articleUrl: element["url"],
+          );
+          news.add(article);
+        }
+      });
+    }
+  }
+}
+
+class NewsForCategorie {
+  List<Article> news = [];
+
+  Future<void> getNewsForCategory(String category) async {
+    /*String url = "http://newsapi.org/v2/everything?q=$category&apiKey=${apiKey}";*/
+    String url =
+        "https://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=$apiKey";
+
+    var response = await http.get(Uri.parse(url));
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          Article article = Article(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            urlToImage: element['urlToImage'],
+            publshedAt: DateTime.parse(element['publishedAt']),
+            content: element["content"],
+            articleUrl: element["url"],
+          );
+          news.add(article);
+        }
+      });
+    }
+  }
+}
---
+++ lib/secret.dart
@@ -0,0 +1,1 @@
+String apiKey = "a40cde7af2574126a6413767e46b6966";
